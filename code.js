c14eb90257eeaab4ac301a8d85fa8f4dd374e86a
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-10-23 15:33:38 -0600

    Update code.js

--- code.js
+++ code.js
@@ -18,14 +18,15 @@ function permutationSort(a) {
     return numPerms;
 }
 
+// Function that creates permutations and then calls the sorted function to check if it's correct
 function perm(a, i) {
     // Check if sorted
     if (sorted(a) == true) {
         return;
     }
     else {
-        for (let j = i; j < a.length; j++) { // Complexity n!, while it runs the whole length of a, it checks every single
-                                            // possible permutation it will have a complexity of n!
+        for (let j = i; j < a.length; j++) { // Complexity nn!, it's worst case would be a run of the length of n, and then
+                                            // it checks every single possible permutation which will have a complexity of n!
             numPerms++;
             [a[i], a[j]] = [a[j], a[i]];
             perm(a, i + 1);
@@ -40,8 +41,9 @@ function perm(a, i) {
 
 // This function checks to see if the array has been sorted
 function sorted(a) {
+    // Run the array and make sure that all the values are in order
     for(let i = 0; i < a.length - 1; i++) { // Complexity n, at worst it will run the entire end of the array
-        // Determine whether or not two values next two each other are sorted, if not it fails
+        // Just compares two values next to each other to make sure the right most one is bigger
         if (a[i] > a[i + 1]) {
             return false;
         }
